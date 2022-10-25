e9d19485f1cff687bc912e3410f65af6d0953e0c
@maruf45 <armaruf450@gmail.com>
2022-10-25 17:12:40 +0600

    init project

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import getAuth from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDPPNKPNovyRtxDbKScel4BwcZizzjHPE4",
+  authDomain: "aurora-ph.firebaseapp.com",
+  projectId: "aurora-ph",
+  storageBucket: "aurora-ph.appspot.com",
+  messagingSenderId: "283121939316",
+  appId: "1:283121939316:web:e2b1ac07b4c9cf5f78b221",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+export default auth;
