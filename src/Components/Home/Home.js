3c5f32c14eed7222c429aced0028fbe925a4732c
@maruf45 <armaruf450@gmail.com>
2022-10-27 00:58:42 +0600

    added home and update home component

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,15 +1,29 @@
-import React from 'react';
-
+import React from "react";
+import "./Home.css";
 const Home = () => {
-    return (
-        <>
-         <div className="home-container">
-            <div className="wrapper-container">
-                
+  return (
+    <>
+      <div className="home-container">
+        <div className="wrapper-container">
+          <div className="home-content">
+            <div className="left-col">
+              <h1>
+                Learning From <br /> Our Course
+              </h1>
+              <p>
+                Welcome to our learning platform. this is grate place to learn 
+                html css javascript, <br /> our course especially create for absolute 
+                beginner. learn anything our course.
+              </p>
             </div>
-         </div>
-        </>
-    );
+            <div className="right-col">
+              <img src="https://i.ibb.co/sF1PtdV/course.png" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
