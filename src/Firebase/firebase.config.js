24737c8c146cbe1e732168708fbf7ce8e5189ce3
@maruf45 <armaruf450@gmail.com>
2022-10-27 11:35:09 +0600

    added env file

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDPPNKPNovyRtxDbKScel4BwcZizzjHPE4",
-  authDomain: "aurora-ph.firebaseapp.com",
-  projectId: "aurora-ph",
-  storageBucket: "aurora-ph.appspot.com",
-  messagingSenderId: "283121939316",
-  appId: "1:283121939316:web:e2b1ac07b4c9cf5f78b221",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 const app = initializeApp(firebaseConfig);
